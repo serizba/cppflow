bffaa732931408195090def1deef65ee80a664df
Khaled BESROUR <khaled.besrour@tessi.fr>
2020-11-19 22:58:36 +0100

    Bug Fix : Remove template structure from cpp to avoid link error

--- src/datatype.cpp
+++ src/datatype.cpp
@@ -63,39 +63,6 @@ namespace cppflow {
     }
 
     /**
-     *
-     * @tparam T
-     * @return The TensorFlow type of T
-     */
-    template<typename T>
-    TF_DataType deduce_tf_type() {
-        if (std::is_same<T, float>::value)
-            return TF_FLOAT;
-        if (std::is_same<T, double>::value)
-            return TF_DOUBLE;
-        if (std::is_same<T, int32_t >::value)
-            return TF_INT32;
-        if (std::is_same<T, uint8_t>::value)
-            return TF_UINT8;
-        if (std::is_same<T, int16_t>::value)
-            return TF_INT16;
-        if (std::is_same<T, int8_t>::value)
-            return TF_INT8;
-        if (std::is_same<T, int64_t>::value)
-            return TF_INT64;
-        if (std::is_same<T, unsigned char>::value)
-            return TF_BOOL;
-        if (std::is_same<T, uint16_t>::value)
-            return TF_UINT16;
-        if (std::is_same<T, uint32_t>::value)
-            return TF_UINT32;
-        if (std::is_same<T, uint64_t>::value)
-            return TF_UINT64;
-
-        throw std::runtime_error{"Could not deduce type!"};
-    }
-
-    /**
      * @return  The stream os after inserting the string representation of dt
      *
      */
