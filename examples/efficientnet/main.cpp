891f68a0715358c972022b3006e0e35a5c3bc043
Jiannan Liu <ljn917@gmail.com>
2021-03-11 05:53:39 -0500

    GPU memory limit example

--- examples/efficientnet/main.cpp
+++ examples/efficientnet/main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
+#include <cstdlib>
 
 #include "cppflow/cppflow.h"
 
+void print_meminfo() {
+    std::system("nvidia-smi");
+}
 
 int main() {
+    // limit GPU memory usage to 30%
+    std::vector<uint8_t> config{0x32,0xb,0x9,0x34,0x33,0x33,0x33,0x33,0x33,0xd3,0x3f,0x20,0x1};
+    TFE_ContextOptions* options = TFE_NewContextOptions();
+    TFE_ContextOptionsSetConfig(options, config.data(), config.size(), cppflow::context::get_status());
+    cppflow::get_global_context() = cppflow::context(options);
+
+    print_meminfo();
 
     auto input = cppflow::decode_jpeg(cppflow::read_file(std::string("../my_cat.jpg")));
     input = cppflow::cast(input, TF_UINT8, TF_FLOAT);
@@ -12,6 +23,8 @@ int main() {
     auto output = model(input);
 
     std::cout << "It's a tiger cat: " << cppflow::arg_max(output, 1) << std::endl;
-    
+
+    print_meminfo();
+
     return 0;
 }
