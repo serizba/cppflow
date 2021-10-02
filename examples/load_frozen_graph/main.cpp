30ceacb810761e8e6b4fe38233c7589c6933641a
paul <bethgeph@gmail.com>
2021-10-02 09:06:41 +0200

    fix example model call

--- examples/load_frozen_graph/main.cpp
+++ examples/load_frozen_graph/main.cpp
@@ -8,7 +8,7 @@ int main() {
 
     auto input = cppflow::fill({10, 5}, 1.0f);
     std::cout << "start" << std::endl;
-    cppflow::model model("../model.pb", {});
+    cppflow::model model("../model.pb", cppflow::model::FROZEN_GRAPH);
     auto output = model({{"x:0", input}}, {{"Identity:0"}})[0];
     
     std::cout << output << std::endl;
